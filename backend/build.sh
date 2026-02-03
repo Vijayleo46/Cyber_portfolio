d8f44f3d4abe83637059029bfd11feb346bfc4c2
Vijayleo46 <vijaymartin72@gmail.com>
2026-02-03 16:17:08 +0530

    fix: explicitly install gunicorn in build.sh and improve path reliability

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
+pip install gunicorn whitenoise
 
 python manage.py collectstatic --no-input
 python manage.py migrate
